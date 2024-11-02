cd500125d7c3dade2913449e762138965156bed5
Siti Anggraini S <sitianggraini.s.02@gmail.com>
2024-11-02 22:48:55 +0700

    progresklmpk.sitiA

---
+++ resources/views/about.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>page about</title>
+
+    <style>
+
+nav {
+    background: rgb(59, 159, 193);
+    padding: 15px 0px;
+    display: grid;
+    place-items: center;
+}
+
+nav > div {
+    font-size: 1.3em;
+    width: 40%;
+    display: flex;
+    justify-content: space-around;
+}
+
+a {
+    color: white;
+    text-decoration: none;
+}
+
+section > div {
+    margin: auto;
+    width: 45%;
+}
+
+    </style>
+</head>
+<body>
+    
+    <nav>
+        <div>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/blog">Blog</a>
+            <a href="/contact">contact</a>
+        </div>
+    </nav>
+   
+    <br><br>
+
+
+    <section>
+        <div>
+            <h1 style="text-align: center;">Tentang Universitas BSI</h1>
+            <p style="font-size: 1.2em; text-align: justify; padding-top: 15px;"><b>Kuliah…? BSI Aja!!</b> Slogan ini pasti sudah melekat di hati dan pikiran sebagian besar masyarakat di Indonesia. Tentunya hal ini mendorong BSI (Bina Sarana Informatika) untuk selalu memperbaiki diri di segala bidang.
+
+                Sejak awal berdirinya Bina Sarana Informatika pada 03 Maret 1988 lalu, sudah 33 tahun BSI berkiprah membantu pemerintah Republik Indonesia dalam rangka mencerdaskan kehidupan bangsa.
+                
+                Dimulai dari LPK BSI (Lembaga Pendidikan Komputer), menjadi Program Satu Tahun, kemudian berevolusi lagi menjadi Program Diploma Tiga: Akademi Manajemen Informatika dan Komputer (AMIK), Akademi Sekretari (ASM), Akademi Bahasa Asing (ABA), Akademi Komunikasi (AKOM), Akademi Manajemen dan Keuangan (AMK).
+                
+                Dan pada tahun akademik 2018/2019, bertepatan dengan kegiatan Seminar Motivasi bagi mahasiswa baru Akademi-akademi BSI, bertempat di Gedung BSI Convention Center, BSI berevolusi menjadi Universitas BSI dengan menggabungkan semua Akademi-akademi yang berada di bawah naungan Yayasan Bina Sarana Informatika.
+                
+                Surat Keputusan Perubahan Akademi-akademi BSI menjadi Universitas BSI diserahkan langsung oleh Sekretaris Jenderal Kementerian Riset, Teknologi dan Pendidikan yang dihadiri pula oleh Direktur Jenderal Pembelajaran dan Kemahasiswaan, Kepala Lembaga Layanan Pendidikan Tinggi Wilayah III DKI Jakarta.
+                
+                Universitas BSI, saat ini BSI memiliki 3 (tiga) Fakultas dengan 22 (duapuluh dua) Program Studi dengan jenjang Diploma Tiga (D3) dan Strata Satu (S1), dan saat ini sedang mempersiapkan Program Magister Management (S2).
+                
+                Dengan penyatuan menjadi Universitas BSI, maka semua kampus BSI yang berada di beberapa kota di Indonesia menjadi satu kesatuan dibawah Universitas Bina Sarana Informatika.</p>
+        </div>
+    </section>
+</body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>artikel</title>
+
+    <style>
+
+        nav {
+            background: rgb(59, 159, 193);
+            padding: 15px 0px;
+            display: grid;
+            place-items: center;
+        }
+        
+        nav > div {
+            font-size: 1.3em;
+            width: 40%;
+            display: flex;
+            justify-content: space-around;
+        }
+        
+        a {
+            color: white;
+            text-decoration: none;
+        }
+        
+        section > div {
+            margin: auto;
+            width: 45%;
+        }
+        
+            </style>
+</head>
+<body>
+
+    <nav>
+        <div>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/blog">Blog</a>
+            <a href="/contact">contact</a>
+        </div>
+    </nav>
+
+    <h2>Ini adalah halaman contact</h2>
+    
+</body>
+</html>
